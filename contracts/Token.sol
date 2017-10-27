b755fb6be4429b7fac5795f29b6fc27e455f12cc
chai2 <chaitulittle@gmail.com>
2017-10-27 02:52:00 -0700

    added base functions for tokens and crowdsale per requirement

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,9 +2,6 @@ pragma solidity ^0.4.15;
 
 import './interfaces/ERC20Interface.sol';
 
-import 'zeppelin-solidity/contracts/crowdsale/Crowdsale.sol';
-import 'zeppelin-solidity/contracts/token/MintableToken.sol';
-
 /**
  * @title Token
  * @dev Contract that implements ERC20 token standard
@@ -15,16 +12,51 @@ contract Token is ERC20Interface {
 	string public name = "Zeus";
 	string public symbol = "Zeus";
 	uint8 public demical = 18;
+	uint256 public totalSupply = 48000;
+	
+	event Mint(address indexed to, uint256 amount);
+	event Burn(address indexed burner, uint256 value);
+	event MintFinished();
 
-	// uint256 public totalSupply = 36000;
+	bool public mintingFinished = false;
+
+	modifier canMint() {
+    	require(!mintingFinished);
+    	_;
+  	}
+
+  	function mint(address _to, uint256 _amount) onlyOwner canMint public returns (bool) {
+	    if(_amount > totalSupply){
+	    	finishMinting();
+	    	return;
+	    }
+	    totalSupply = totalSupply.add(_amount);
+	    balances[_to] = balances[_to].add(_amount);
+	    Mint(_to, _amount);
+	    Transfer(address(0), _to, _amount);
+	    return true;
+  	}
+
+  	function finishMinting() onlyOwner public returns (bool) {
+    	mintingFinished = true;
+    	MintFinished();
+    	return true;
+  	}
+
+  	function burn(uint256 _value) public {
+        require(_value > 0);
+        require(_value <= balances[msg.sender]);
+
+        address burner = msg.sender;
+        balances[burner] = balances[burner].sub(_value);
+        totalSupply = totalSupply.sub(_value);
+        Burn(burner, _value);
+    }
 
 	// bool saleEnded;
 
 	// address public minter;
 	// mapping (address => uint) public balances;
-
-	// event Purchase(address from, address to, unint amount);
-	// event Refund(address from, address to, uint amount);
 
 	// function Coin() {
 	// 	minter = msg.sender;
