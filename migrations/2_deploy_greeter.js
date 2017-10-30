673f177012119b474df6adc5807f31da287a16e2
chai2 <chaitulittle@gmail.com>
2017-10-29 22:48:28 -0700

    changes to deployer

--- migrations/2_deploy_greeter.js
+++ migrations/2_deploy_greeter.js
@@ -2,26 +2,23 @@ var Crowdsale = artifacts.require("./Crowdsale.sol");
 var Queue = artifacts.require("./Queue.sol");
 var Token = artifacts.require("./Token.sol");
 
-module.exports = function(deployer, network, accounts) {
+module.exports = function(deployer) {
   return liveDeploy(deployer);
 };
 
 async function liveDeploy(deployer){
-  crowdSaleInfo(web3.eth.accounts[0]);
-  return deployer.deploy(Crowdsale, totalSupply, startTime, endTime, rate, owner, maxTime).then(async () => {
+  const owner = web3.eth.accounts[0];
+  const totalSupply = 48000;
+  const maxTime = 10;
+  const startTime = latestTime();
+  const endTime =   startTime + 50000;
+  const rate = web3.toWei('0.1', 'ether');
+
+  return deployer.deploy(Crowdsale, 48000, startTime, endTime, rate, owner, maxTime).then(async () => {
     const instance = await Crowdsale.deployed();
     const token = await instance.token.call();
     console.log('Token Address', token);
   });
-}
-
-function crowdSaleInfo(owner_account){
-  const owner = owner_account;
-  const totalSupply = 48000;
-  const maxTime = 10;
-  const startTime = latestTime() + duration.weeks(1);
-  const endTime =   startTime + duration.weeks(1);
-  const rate = new BigNumber(10);
 }
 
 function latestTime() {
--- truffle.js
+++ truffle.js
@@ -6,5 +6,8 @@ module.exports = {
       port: 8545,
       network_id: "*" // Match any network id
     }
+  },
+  "scripts": {
+  	"start": "webpack-dev-server"
   }
 };
